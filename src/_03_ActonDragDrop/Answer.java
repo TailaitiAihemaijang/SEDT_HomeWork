c9787bccae87ffd3a729c2b5ead66e644600466f
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-08-25 07:20:14 +0300

    Action DragDrop soru 2   http://dhtmlgoodies.com/scripts/drag-drop-nodes/drag-drop-nodes.html           buradaki bütün öğrencileri bütün kutucukları doldurana kadar atınız.

--- src/_03_ActonDragDrop/Answer.java
+++ src/_03_ActonDragDrop/Answer.java
@@ -26,7 +26,7 @@ public class Answer extends BaseDriver {
             WebElement answer = answers.get(i);
             for (int j = 0; j < questions.size(); j++) {
                 WebElement question = questions.get(j);
-                if (!question.getAttribute("class").contains("correctAnswer")){
+                if (!question.getAttribute("class").contains("correctAnswer")) {
                     actions.moveToElement(answer).clickAndHold().build().perform();
                     MyFunc.Wait(2);
                     actions.moveToElement(question).release().build().perform();
@@ -39,10 +39,52 @@ public class Answer extends BaseDriver {
                     actions.moveToElement(answer).clickAndHold().build().perform();
                     MyFunc.Wait(2);
                     actions.moveToElement(question).release().build().perform();
-                }else break;
+                } else break;
 
 
             }
+        }
+        bekleVeKapat();
+    }
+
+    @Test
+    public void Test2() {
+        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-nodes/drag-drop-nodes.html");
+        Actions actions = new Actions(driver);
+        List<WebElement> sutudetns = driver.findElements(By.id("allItems"));
+        WebElement box1 = driver.findElement(By.id("box1"));
+        WebElement box2 = driver.findElement(By.id("box2"));
+        WebElement box3 = driver.findElement(By.id("box3"));
+        WebElement box4 = driver.findElement(By.id("box4"));
+        WebElement box5 = driver.findElement(By.id("box5"));
+        for (WebElement student : sutudetns) {
+
+            for (int i = 1; i < 5; i++) {
+                actions.moveToElement(student).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box1).release().build().perform();
+            }
+            for (int i = 1; i < 5; i++) {
+                actions.moveToElement(student).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box2).release().build().perform();
+            }
+            for (int i = 1; i < 5; i++){
+                actions.moveToElement(student).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box3).release().build().perform();
+            }
+            for (int i = 1; i < 4; i++) {
+                actions.moveToElement(student).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box4).release().build().perform();
+            }
+            for (int i = 1; i < 7; i++) {
+                actions.moveToElement(student).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box5).release().build().perform();
+            }
+
         }
         bekleVeKapat();
     }
