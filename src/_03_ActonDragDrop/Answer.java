47a931df24e77e850fc8b702ba386713015adf51
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-08-25 09:32:01 +0300

    Action DragDrop soru 3   http://dhtmlgoodies.com/scripts/drag-drop-nodes-quiz/drag-drop-nodes-quiz.html            buradaki bütün şehirleri bütün ülkere doğru şekilde topluca dağıtınız.

--- src/_03_ActonDragDrop/Answer.java
+++ src/_03_ActonDragDrop/Answer.java
@@ -69,7 +69,7 @@ public class Answer extends BaseDriver {
                 MyFunc.Wait(1);
                 actions.moveToElement(box2).release().build().perform();
             }
-            for (int i = 1; i < 5; i++){
+            for (int i = 1; i < 5; i++) {
                 actions.moveToElement(student).clickAndHold().build().perform();
                 MyFunc.Wait(1);
                 actions.moveToElement(box3).release().build().perform();
@@ -89,5 +89,114 @@ public class Answer extends BaseDriver {
         bekleVeKapat();
     }
 
+    @Test
+    public void Test3() {
+        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-nodes-quiz/drag-drop-nodes-quiz.html");
+        List<WebElement> citys = driver.findElements(By.id("allItems"));
+        Actions actions = new Actions(driver);
+        WebElement box1 = driver.findElement(By.id("box1"));
+        WebElement box2 = driver.findElement(By.id("box2"));
+        WebElement box3 = driver.findElement(By.id("box3"));
+        WebElement box4 = driver.findElement(By.id("box4"));
+        WebElement box5 = driver.findElement(By.id("box5"));
+        WebElement box6 = driver.findElement(By.id("box6"));
+        WebElement box7 = driver.findElement(By.id("box7"));
+        for (WebElement city : citys) {
+            MyFunc.Wait(1);
+
+            for (int i = 1; i < 8; i++) {
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box1).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box2).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box3).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box4).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box5).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box6).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) {
+                    break;
+                } else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                MyFunc.Wait(1);
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box7).release().build().perform();
+            }
+
+        }
+        bekleVeKapat();
+    }
+
+    @Test
+    public void Test4() {
+        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-nodes-quiz/drag-drop-nodes-quiz.html");
+        Actions actions = new Actions(driver);
+        for (int i = 1; i <= 21; i++) {
+            String stringId = "node" + i;
+            WebElement city = driver.findElement(By.id(stringId));
+            for (int j = 1; j <= 7; j++) {
+                String stringBox = "box" + j;
+                WebElement box = driver.findElement(By.id(stringBox));
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box).release().build().perform();
+                if (city.getAttribute("class").contains("correctAnswer")) break;
+                else if (city.getAttribute("class").contains("wrongAnswer")) {
+                    continue;
+                }
+                actions.moveToElement(city).clickAndHold().build().perform();
+                MyFunc.Wait(1);
+                actions.moveToElement(box).release().build().perform();
+            }
+        }
+
+        bekleVeKapat();
+
+
+    }
 
 }
+
+
+
